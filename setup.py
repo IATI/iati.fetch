c727d820847cd1e920d97b9b1bf6f475d71c082f
dalepotter <dalepotter@gmail.com>
2017-06-14 15:31:52 +0100

    Remove unnecessary comment

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     url='http://iatistandard.org/',
     packages = find_packages(exclude='iati/fetch/tests'),
     install_requires = [
-        'iati.core==0.1dev'
+        'iati.core==0.1dev',
+        'requests'
         ],
     dependency_links=['https://github.com/IATI/iati.core/tarball/master#egg=iati.core-0.1dev'],
     classifiers = [
