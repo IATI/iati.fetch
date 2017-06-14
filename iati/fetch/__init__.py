e5055265288b398db728c1c6d43f022bcd70321f
Imogen Kutz <imogenk@Imogens-MacBook-Pro.local>
2017-06-14 15:46:48 +0100

    Fix linting errors

--- iati/fetch/__init__.py
+++ iati/fetch/__init__.py
@@ -1,9 +1,13 @@
-"""A module containing functionality for iati.fetch"""
+"""A module containing functionality for iati.fetch."""
 import json
 import requests
 
+
 class Dataset(object):
+    """Container class to fetch a dataset."""
+
     def __init__(self, dataset_id):
+        """Fetch a dataset by registry dataset ID."""
         req = requests.get("https://iatiregistry.org/api/3/action/package_show?id={0}".format(dataset_id))
 
         if req.status_code != 200:
--- iati/fetch/tests/test_dataset.py
+++ iati/fetch/tests/test_dataset.py
@@ -1,14 +1,13 @@
-"""A module containing tests for the library functionality to fetch datasets"""
-import pytest
+"""A module containing tests for the library functionality to fetch datasets."""
 import iati.core
 import iati.fetch
 
+
 class TestDatset(object):
-    """Container for tests relating fetch.Dataset"""
+    """Container for tests relating fetch.Dataset."""
 
     def test_fetch_by_dataset_id(self):
         """Given a dataset_id, metadata for this dataset will be populated."""
-
         sample_dataset = iati.fetch.Dataset(dataset_id='43aa0616-58a4-4d16-b0a9-1181e3871827')
         sample_dataset = iati.fetch.Dataset(dataset_id='mock_dataset_id')
 
