95b695ceb2c5fda603abda9cbce4a5109e06ef6e
allthatilk <allthatilk@users.noreply.github.com>
2017-06-15 14:38:57 +0100

    Refactoring and add mock of registry metadata

--- iati/fetch/__init__.py
+++ iati/fetch/__init__.py
@@ -9,31 +9,39 @@ class Dataset(object):
 
     def __init__(self, dataset_id):
         """Fetch a dataset by registry dataset ID."""
-        registry_metadata = self.get_metadata(dataset_id)
-        dataset_url = registry_metadata["result"]["resources"][0]["url"]
-        dataset = self.get_dataset(dataset_url)
+        self.dataset_id = dataset_id
+        self.dataset = None
+
+    def set_dataset(self):
+        """Set dataset from the dataset ID."""
+        registry_metadata = get_metadata(self.dataset_id)
+        dataset_url = registry_metadata['result']['resources'][0]['url']
+        dataset = get_dataset(dataset_url)
         self.dataset = iati.core.data.Dataset(dataset)
 
-    def get_metadata(self, dataset_id):
-        """Get registry metadata for given dataset ID."""
-        req = requests.get("https://iatiregistry.org/api/3/action/package_show?id={0}".format(dataset_id))
-        if not self.is_response_okay(req):
-            raise Exception()
-        else:
-            registry_metadata = json.loads(req.text)
-            return registry_metadata
 
-    def get_dataset(self, dataset_url):
-        """Get dataset from dataset URL."""
-        dataset = requests.get(dataset_url)
-        if not self.is_response_okay(dataset):
-            raise Exception()
-        else:
-            return dataset.text.encode("utf-8")
+def get_metadata(dataset_id):
+    """Get registry metadata for given dataset ID."""
+    req = requests.get("https://iatiregistry.org/api/3/action/package_show?id={0}".format(dataset_id))
+    if not is_response_okay(req):
+        raise Exception()
+    else:
+        registry_metadata = json.loads(req.text)
+        return registry_metadata
 
-    def is_response_okay(self, request):
-        """Check for network failure."""
-        if request.status_code != 200:
-            return False
-        else:
-            return True
+
+def get_dataset(dataset_url):
+    """Get dataset from dataset URL."""
+    dataset = requests.get(dataset_url)
+    if not is_response_okay(dataset):
+        raise Exception()
+    else:
+        return dataset.text.encode('utf-8')
+
+
+def is_response_okay(request):
+    """Check for network failure."""
+    if request.status_code != 200:
+        return False
+    else:
+        return True
--- iati/fetch/tests/test_dataset.py
+++ iati/fetch/tests/test_dataset.py
@@ -1,14 +1,21 @@
 """A module containing tests for the library functionality to fetch datasets."""
-import iati.core
 import iati.fetch
+import pkg_resources
 import requests_mock
 
 
-class TestDatset(object):
+class TestDataset(object):
     """Container for tests relating fetch.Dataset."""
 
     def test_fetch_by_dataset_id(self):
         """Given a dataset_id, metadata for this dataset will be populated."""
         sample_dataset = iati.fetch.Dataset(dataset_id='43aa0616-58a4-4d16-b0a9-1181e3871827')
-
+        sample_dataset.set_dataset()
         assert isinstance(sample_dataset.dataset, iati.core.data.Dataset)
+
+    def test_get_metadata(self):
+        """Given a mock registry ID, metadata will be returned."""
+        with requests_mock.Mocker() as m:
+            mock_Metadata = pkg_resources.resource_stream(__name__, 'mock-registry-metadata.json').read().decode()
+            m.get('https://iatiregistry.org/api/3/action/package_show?id=sample', text=mock_Metadata)
+            assert iati.fetch.get_metadata(dataset_id='sample') == {'result': {'resources': [{'url': 'https://www.vsointernational.org/sites/default/files/aasaman_gec.xml'}]}}
