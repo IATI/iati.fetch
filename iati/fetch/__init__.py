7f82987f3bd8c01e52b5edf1345cdc92841d9cc5
allthatilk <allthatilk@users.noreply.github.com>
2017-06-15 10:48:44 +0100

    Functionality for fetching valid dataset

--- iati/fetch/__init__.py
+++ iati/fetch/__init__.py
@@ -1,4 +1,5 @@
 """A module containing functionality for iati.fetch."""
+import iati.core.data
 import json
 import requests
 
@@ -8,10 +9,31 @@ class Dataset(object):
 
     def __init__(self, dataset_id):
         """Fetch a dataset by registry dataset ID."""
-        req = requests.get("https://iatiregistry.org/api/3/action/package_show?id={0}".format(dataset_id))
+        registry_metadata = self.get_metadata(dataset_id)
+        dataset_url = registry_metadata["result"]["resources"][0]["url"]
+        dataset = self.get_dataset(dataset_url)
+        self.dataset = iati.core.data.Dataset(dataset)
 
-        if req.status_code != 200:
+    def get_metadata(self, dataset_id):
+        """Get registry metadata for given dataset ID."""
+        req = requests.get("https://iatiregistry.org/api/3/action/package_show?id={0}".format(dataset_id))
+        if not self.is_response_okay(req):
             raise Exception()
         else:
             registry_metadata = json.loads(req.text)
-        pass
+            return registry_metadata
+
+    def get_dataset(self, dataset_url):
+        """Get dataset from dataset URL."""
+        dataset = requests.get(dataset_url)
+        if not self.is_response_okay(dataset):
+            raise Exception()
+        else:
+            return dataset.text.encode("utf-8")
+
+    def is_response_okay(self, request):
+        """Check for network failure."""
+        if request.status_code != 200:
+            return False
+        else:
+            return True
--- iati/fetch/tests/test_dataset.py
+++ iati/fetch/tests/test_dataset.py
@@ -1,6 +1,7 @@
 """A module containing tests for the library functionality to fetch datasets."""
 import iati.core
 import iati.fetch
+import requests_mock
 
 
 class TestDatset(object):
@@ -9,6 +10,5 @@ class TestDatset(object):
     def test_fetch_by_dataset_id(self):
         """Given a dataset_id, metadata for this dataset will be populated."""
         sample_dataset = iati.fetch.Dataset(dataset_id='43aa0616-58a4-4d16-b0a9-1181e3871827')
-        sample_dataset = iati.fetch.Dataset(dataset_id='mock_dataset_id')
 
-        assert isinstance(sample_dataset, iati.core.Dataset)
+        assert isinstance(sample_dataset.dataset, iati.core.data.Dataset)
